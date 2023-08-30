8adfd42caf33638317074fa554540a1b0805e08c
TeresaTao127 <taoleiluck2020@gmail.com>
2023-08-30 09:27:37 +0100

    L4 task8 revise updated in separate test

--- src/test/java/lesson4Task8/PersonTest.java
+++ src/test/java/lesson4Task8/PersonTest.java
@@ -1,17 +1,22 @@
 package lesson4Task8;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
     @org.junit.jupiter.api.Test
-    void testEquals() {
+    void shouldReturnFalseForEqualMethod() {
         Person person1 = new Person("John", 28, "Male", "Ireland");
         Person person2 = new Person("Selina", 25, "Female", "UK");
         boolean expectedResult = false;
         boolean result = person1.equals(person2);
         assertEquals(expectedResult, result);
+    }
 
+    @org.junit.jupiter.api.Test
+        void shouldReturnTrueForEqualMethod() {
         Person person3 = new Person("Mike", 30, "Male", "China");
         Person person4 = new Person("Mike", 30, "Male", "China");
         boolean expectedResult1=true;
